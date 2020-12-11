4f47ac5ba697e6519d5714b32910512b86e239fe
bndnt <l.makushchenko@gmail.com>
2020-12-11 16:07:36 +0200

    добавили инфинити и -инфинити

--- firstHometask.js
+++ firstHometask.js
@@ -5,10 +5,13 @@ let isProgrammer = true;
 let is20Years=false;
 let kids= null;
 let wife;
-let a=Math.sqrt(2,53-1);
-let b=Math.sqrt(2,54);
-let c=-Math.sqrt(2,53-1);
-let d=-Math.sqrt(2,54);
+let x=(1/0);
+let y=(-1/0);
+let a=Math.pow(2,53)-1;
+let b=Math.pow(2,53);
+let c=-(Math.pow(2,53)-1);
+let d=-(Math.pow(2,53)-1);
+
 //выводим  значения  наших переменных
 console.log(name);
 console.log(age);
@@ -20,3 +23,19 @@ console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
+console.log(x);
+console.log(y);
+
+//выводим тип данных каждой переменной
+console.log( typeof name);
+console.log(typeof age);
+console.log(typeof isProgrammer);
+console.log(typeof is20Years);
+console.log(typeof kids);
+console.log(typeof wife);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof x);
+console.log(typeof y);

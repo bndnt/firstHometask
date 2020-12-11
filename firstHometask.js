782507d4a552d2b9389a520c7ad9fd1b63f3c8cc
bndnt <l.makushchenko@gmail.com>
2020-12-11 15:47:25 +0200

    поправки

--- firstHometask.js
+++ firstHometask.js
@@ -5,11 +5,18 @@ let isProgrammer = true;
 let is20Years=false;
 let kids= null;
 let wife;
-
-//выводим тип данных наших переменных
+let a=Math.sqrt(2,53-1);
+let b=Math.sqrt(2,54);
+let c=-Math.sqrt(2,53-1);
+let d=-Math.sqrt(2,54);
+//выводим  значения  наших переменных
 console.log(name);
 console.log(age);
 console.log(isProgrammer);
 console.log(is20Years);
 console.log(kids);
 console.log(wife);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);

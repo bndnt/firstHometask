b50b925ed4e1c5ed9b7d0720c6042552c1a38068
bndnt <l.makushchenko@gmail.com>
2020-12-11 15:28:09 +0200

    Create file firstHometask.js
    Выполнили первое задание по созданию элементов с разными типами данных

---
+++ firstHometask.js
@@ -0,0 +1,9 @@
+//сознаем n-е количество элементов с разными типами данных
+let name ="Иван";
+let age = 25;
+let isProgrammer = true;
+let is20Years=false;
+let kids= null;
+let wife;
+
+//выводим тип данных наших переменных
